cafbb46638154f31b14421a8401e5cdd7567bfe1
Bram <brampijper@gmail.com>
2023-04-20 15:06:41 +0200

    imported cors as a seperate module

---
+++ utils/cors.js
@@ -0,0 +1,15 @@
+const cors = require('cors')
+
+const whitelist = process.env.WHITELISTED.split(", ");
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+module.exports = cors(corsOptions);
