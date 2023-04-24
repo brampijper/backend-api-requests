517d3c23ea75f35c9ed15cd71365d9080d609c29
Bram <brampijper@gmail.com>
2023-04-24 14:08:04 +0200

    restructured routes: only sending back the needed data

--- routes/github.js
+++ routes/github.js
@@ -12,58 +12,75 @@ router.use((req, res, next) => {
 
 // define github/repos route
 router.get('/repos', cors, (req, res) => {
-    console.log('repo accessed')
-    const octokit = new Octokit({
-      auth: process.env.GITHUB_API_KEY,
-      userAgent: "brampijper",
-      Accept: "application/vnd.github.16.28.4.raw",
-    });
+  const username = req.query.username
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_API_KEY,
+    userAgent: username,
+    Accept: "application/vnd.github.16.28.4.raw",
+  });
   
-    octokit.rest.repos.listForUser({
-      username: "brampijper",
-    })
-      .then( response => res.send(response.data))
-      .catch( error => console.error(error))
+  const response = octokit.rest.repos.listForUser({username});
+  response
+    .then( ({data}) => {
+      const repositories = data.filter( repo => repo.homepage)
+      res.send(repositories)
+  }).catch(error => console.error('Error ', error))
 })
 
 // define user stats route for github
 router.get('/stats', cors, (req, res) => {
-    console.log('stats accessed')
+  const username = req.query.username
     
-    const headers = {
-        'Authorization': ` bearer ${process.env.GITHUB_API_KEY}`,
-    }
-    const body = {
-        "query": `query {
-            user(login: "brampijper") {
-              name
-              contributionsCollection {
-                contributionCalendar {
-                  colors
-                  totalContributions
-                  weeks {
-                    contributionDays {
-                      color
-                      contributionCount
-                      date
-                      weekday
-                    }
-                    firstDay
+  const headers = {
+      'Authorization': ` bearer ${process.env.GITHUB_API_KEY}`,
+  }
+
+  const body = {
+      "query": `query {
+          user(login: "${username}") {
+            name
+            contributionsCollection {
+              contributionCalendar {
+                colors
+                totalContributions
+                weeks {
+                  contributionDays {
+                    color
+                    contributionCount
+                    date
+                    weekday
                   }
+                  firstDay
                 }
               }
             }
-        }`
-    }
+          }
+      }`
+  }
     
-    fetch('https://api.github.com/graphql', { 
-          method: 'POST', 
-          body: JSON.stringify(body), 
-          headers: headers 
-    })
+  const response = fetch('https://api.github.com/graphql', { 
+    method: 'POST', 
+    body: JSON.stringify(body), 
+    headers: headers 
+  })
+
+  response
     .then( data => data.json())
-    .then( data => res.send(data))
+    .then( ({data}) => {
+      const { 
+        user: { 
+          contributionsCollection: { 
+            contributionCalendar: { 
+              totalContributions 
+            }
+          }
+        }  
+      } = data;
+      res.send(totalContributions.toString()) // integer is not valid to send with Express
+    })
     .catch( err => console.error('error while fetching ', err))
+
 })
 
 module.exports = router;
