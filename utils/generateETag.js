ac9354ec476af363c0976f7064d6aae923d8e190
Bram <brampijper@gmail.com>
2023-04-25 21:05:47 +0200

    added: function for generating etags

---
+++ utils/generateETag.js
@@ -0,0 +1,6 @@
+// Generate ETag from the data
+module.exports = function(data) {
+    const serializedData = JSON.stringify(data);
+    const hash = require('crypto').createHash('sha1').update(serializedData).digest('hex');
+    return `"${hash}"`;
+}
