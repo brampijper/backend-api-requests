6eae3c5dbf1b464ce3dff21c2e30df476a19a8de
Bram <brampijper@gmail.com>
2023-04-28 17:19:08 +0200

    renamed routes

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express')
 require('dotenv').config()
-const github = require('./routes/github');
+
+// Routes
+const repos = require('./routes/github/repos');
+const stats = require('./routes/github/stats');
 const blog = require('./routes/blog');
 
-const app = express() // "check /bin/www.js" for port & settings 
+// Check "/bin/www.js" for port & settings.
+const app = express() 
+
+app.use((req, res, next) => {
+  
+  // setting corrs to the most flexible settings, while developing.
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Expose-Headers', '*')
+
+  next()
+})
 
 app.get('/', (req,res) => {
   res.json('hi, this is the home of the server')
-
 })
 
-app.use('/api', github)
+
+app.use('/api', repos)
+app.use('/api', stats)
 app.use('/blog', blog)
 
 module.exports = app;
