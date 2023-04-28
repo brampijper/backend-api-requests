ac503132b9c795a34545441a8cbb3f86288717e3
Bram <brampijper@gmail.com>
2023-04-28 17:32:18 +0200

    removed cors

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ const blog = require('./routes/blog');
 // Check "/bin/www.js" for port & settings.
 const app = express() 
 
-app.use((req, res, next) => {
-  
-  // setting corrs to the most flexible settings, while developing.
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  res.setHeader('Access-Control-Expose-Headers', '*')
-
-  next()
-})
-
 app.get('/', (req,res) => {
   res.json('hi, this is the home of the server')
 })
