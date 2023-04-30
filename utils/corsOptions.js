469cf7dcffb58c6353b04e95244d6a0bad421968
Bram <brampijper@gmail.com>
2023-04-30 20:43:33 +0200

    removed console.log

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -2,7 +2,6 @@ const whitelist = process.env.WHITELISTED.split(", ");
 // console.log(whitelist)
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log(whitelist)
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
