d87b359c30a178f772664dfb39db2cd62fb0b799
Bram <brampijper@gmail.com>
2023-04-30 20:42:08 +0200

    Implemented puppeteer to take screenshots for my websites  when fetching data from github.

---
+++ utils/screenshot.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+
+async function takeScreenshot(url, dir) {
+    const screenshotName = `${url.replace(/[^a-zA-Z0-9]/g, '-')}.jpg`;
+    const screenshotPath = path.join(dir, screenshotName);
+    
+    if (fs.existsSync(screenshotPath)) {
+        // console.log(`Screenshot for ${url} already exists, skipping...`);
+        return screenshotName;
+    }
+  
+    const browser = await puppeteer.launch({headless: "new"});
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.setViewport({
+        width: 0,
+        height: 800,
+        deviceScaleFactor: 1,
+      });
+      
+    await page.screenshot({ 
+        path: screenshotPath, 
+        // fullPage: true,
+        type: 'jpeg',
+        quality: 80, 
+    });
+    
+    await browser.close();
+  
+    // console.log(`Screenshot for ${url} created at ${screenshotPath}`);
+    return screenshotName;
+}
+
+// takeScreenshot('https://www.google.com', './screenshots');
+
+module.exports = takeScreenshot
