ae2b0d011a35be1da2ec9a254ef916f923394126
Bram <brampijper@gmail.com>
2023-05-02 20:30:03 +0200

    swapped puppeteer for webdriver

--- utils/screenshot.js
+++ utils/screenshot.js
@@ -1,57 +1,45 @@
-const puppeteer = require('puppeteer');
+const webdriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
 const fs = require('fs');
 const path = require('path');
 
 async function takeScreenshot(url, dir) {
-    try {
-        const screenshotName = url
-            .replace(/^https?:\/\//i, '') // remove "http://" or "https://"
-            .replace(/[^a-zA-Z0-9]/g, '-') // replace non-alphanumeric characters with hyphen "-"
-            + '.jpg'; // add jpg extension.
-        const screenshotPath = path.join(dir, screenshotName);
-        console.log('screenshot path: ', screenshotPath)
-        if (fs.existsSync(screenshotPath)) {
-            console.log(`Screenshot for ${url} already exists, skipping...`);
-            return screenshotName;
-        }
+  const driver = await new webdriver.Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(new firefox.Options().headless())
+    .build();
 
-        let browser;
-        console.log(process.env.PUPPETEER_EXECUTABLE_PATH)
-        browser = await puppeteer.launch({
-            // headless: true,
-            // env: { DISPLAY: ":10"},
-            // executablePath: "C:\Program Files\Google\Chrome\Application\chrome.exe",
-            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless'], // required
-        });
-      
-        const page = await browser.newPage();
-        await page.goto(url);
-    
-        await page.setViewport({
-            width: 0,
-            height: 800,
-            deviceScaleFactor: 1,
-          });
-          
-        await page.screenshot({ 
-            path: screenshotPath, 
-            // fullPage: true,
-            type: 'jpeg',
-            quality: 80, 
-        });
-        
-        await browser.close();
-      
-        console.log(`Screenshot for ${url} created at ${screenshotPath}`);
-        return screenshotName;
-    } catch (err) {
-        
-        console.log(`Error occurred while taking screenshot of ${url}`, err); // Handle any errors that may occur during the function execution
-        throw err; // rethrow the error to be handled by the caller of this function
+  try {
+    const screenshotName = url
+      .replace(/^https?:\/\//i, '')
+      .replace(/[^a-zA-Z0-9]/g, '-')
+      + '.jpg';
+    const screenshotPath = path.join(dir, screenshotName);
+    if (fs.existsSync(screenshotPath)) {
+      console.log(`Screenshot for ${url} already exists, skipping...`);
+      return screenshotName;
     }
+
+    await driver.get(url);
+    await driver.manage().window().setRect({ width: 800, height: 1000 });
+    await driver.wait(async () => {
+      const readyState = await driver.executeScript('return document.readyState');
+      return readyState === 'complete';
+    }, 10000);
+    const screenshot = await driver.takeScreenshot();
+    const buffer = Buffer.from(screenshot, 'base64');
+    await fs.promises.writeFile(screenshotPath, buffer);
+
+    console.log(`Screenshot for ${url} created at ${screenshotPath}`);
+    return screenshotName;
+  } catch (err) {
+    console.log(`Error occurred while taking screenshot of ${url}`, err);
+    throw err;
+  } finally {
+    await driver.quit();
+  }
 }
 
 // takeScreenshot('https://www.google.com', './screenshots');
 
-module.exports = takeScreenshot
+module.exports = takeScreenshot;
