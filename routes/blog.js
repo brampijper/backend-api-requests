4b8fd18775b8f8840fb54293907709c95cf40d1b
Bram <brampijper@gmail.com>
2023-05-11 19:54:39 +0200

    Fix: caching now works properly

--- routes/blog.js
+++ routes/blog.js
@@ -8,68 +8,66 @@ const {
     isCachedDataValid, 
     storeData, 
     getCachedData 
-  } = require('../utils/isCachedDataValid');
+  } = require('../utils/cacheUtils');
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
-    console.log('blog time: ', Date.now())
+    // console.log('blog time: ', Date.now())
     next()
 })
 
 router.get('/posts', (req, res) => {
-
+    const expirationInMinutes = 1 // set custom expire time for this route.
+    const maxAgeInSeconds = expirationInMinutes * 60;
+  
     // Check if the ETag is present in the request headers
-    const etag = req.headers['if-none-match'];
-    // console.log(etag)
-
-    if (etag) { //etag exists. Let's compare it with the cached server data.
-        const isValid = isCachedDataValid('/posts', etag)
-    
-        if (isValid) { // Use the cached data on the client side.
-          console.log('posts: isvalid: ', isValid)
-          res.status(304).end();
-          return;
-        }
+    const clientEtag = req.headers['if-none-match']
+    console.log('clientEtag in posts: ', clientEtag)
+  
+    if (clientEtag) { //etag exists. Let's compare it with the cached server data.
+      const isValid = isCachedDataValid('/posts', clientEtag)
+      console.log('posts: isvalid: ', isValid);
+  
+      if (isValid) { // Use the cached data on the client side.
+        console.log('posts: isvalid: ', isValid)      
+        res.status(304).end();
+        return;
       }
-
-    else {
-        const cachedDataOnServer = getCachedData('/posts'); // check for the cached data on the server with the correct path.
-    
-        // ok. no cached no data on the client side ? Here. have the cached data from the server.
-        if (cachedDataOnServer) {
-          console.log( 'cached data on server', cachedDataOnServer);  
-          const { cachedData, cachedEtag } = cachedDataOnServer;
-          
-          res.setHeader('Cache-Control', 'public, max-age=604800')
-          res.setHeader('ETag', cachedEtag);
-          res.send(cachedData)
-          return;
-        } 
-
-        (async () => {
-            console.log('blog: making a normal request')
-            let feed = await parser.parseURL('https://focused-galileo-c3ee18.netlify.app/rss.xml');
-            
-            if (feed.items) {
-                const [firstPost] = feed.items;
-    
-                const etag = generateETag(firstPost)
-                
-                storeData('/posts', firstPost, etag)
-
-                res.setHeader('Cache-Control', 'public, max-age=604800')
-                res.setHeader('ETag', etag);
-    
-                res.send(firstPost)
-            }
-            
-            else {
-                const message = `An error has occured: ${feed}`;
-                console.error(message)
-            }
-        })();
     }
 
+    const cachedDataOnServer = getCachedData('/posts'); // check for the cached data on the server with the correct path.
+
+    // ok. no cached no data on the client side ? Here. have the cached data from the server.
+    if (cachedDataOnServer) {
+        console.log('posts: cached data on server');  
+        const { data, etag, expirationTime } = cachedDataOnServer;
+        
+        res.setHeader('Cache-Control', `public, max-age=${expirationTime / 1000}`) // expiration time in seconds.
+        res.setHeader('ETag', etag)
+        res.send(data)
+        return;
+    } 
+
+    (async () => {
+        console.log('blog: making a normal request')
+        let feed = await parser.parseURL('https://focused-galileo-c3ee18.netlify.app/rss.xml');
+        
+        if (!feed.items) {
+            const message = `An error has occured: ${feed}`;
+            console.error(message)
+        }
+
+        const [firstPost] = feed.items;
+
+        const etag = generateETag(firstPost)
+        
+        storeData('/posts', firstPost, etag, maxAgeInSeconds)
+
+        res.setHeader('Cache-Control', `public, max-age=${maxAgeInSeconds}`) // always revalidates with the server
+        res.setHeader('ETag', etag)
+
+        res.send(firstPost)
+    })();
 })
 
 module.exports = router;
