422d51d710aa518dc80e8f5aa2c9a88a5ae42e4b
Bram <brampijper@gmail.com>
2023-05-14 22:39:20 +0200

    extracted old code into controllers

---
+++ controllers/blog-posts.controller.js
@@ -0,0 +1,32 @@
+const { fetchBlogPosts } = require('../services')
+const { isClientDataValid, getCachedData } = require('../utils/cacheHelpers');
+
+const handleGetBlogPosts = async (req, res, next) => {
+    const path = '/posts'
+    const clientEtag = req.headers['if-none-match'] || undefined 
+
+    try {
+        const isDataEqual = await isClientDataValid(path, clientEtag)
+        
+        if (clientEtag && isDataEqual) {
+            res.status(304).end() // data did not change.
+        }
+        
+        const getCachedDataOnServer = await getCachedData(path) // Get cached data on server if exists.
+
+        const { data, etag, expiration} = // Use cached data or fetch fresh data,
+            getCachedDataOnServer ||  (await fetchBlogPosts(path))
+
+        res.setHeader('Cache-Control', `public, max-age=${expiration  / 1000}`); // expiration time in seconds.
+        res.setHeader('ETag', etag);
+        res.send(data);
+
+    }  catch(error) {
+        console.error('error while fetching', error);
+        res.status(500).send('An error occurred while fetching data.');
+    }
+}
+
+module.exports = {
+    handleGetBlogPosts
+}
