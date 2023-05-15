13069487ba1ed3a75b082d86b3e325bccc34253c
Bram <brampijper@gmail.com>
2023-05-15 13:18:59 +0200

    updated: implemented new route system

--- index.js
+++ index.js
@@ -6,28 +6,19 @@ const cors = require('cors');
 const { corsOptions } = require('./utils/corsOptions');
 
 // Routes
-const repos = require('./routes/github/repos');
-const stats = require('./routes/github/stats');
-const blog = require('./routes/blog');
+const router = require('./routes/index');
 
 // Check "/bin/www.js" for port & settings.
-const app = express() 
-
-app.use(express.static(path.join(__dirname, 'files/screenshots'))); // serving static file using  middleware, with the root directory set to ./files.
+const app = express()
 
 // Enable cors with the options object.
 app.use(cors(corsOptions))
 
+app.use(router)
+
+app.use(express.static(path.join(__dirname, 'files/screenshots'))); // serving static file using  middleware, with the root directory set to ./files.
+
 // disable the default etag middleware
 app.set('etag', false);
-
-app.get('/', (req,res) => {
-  res.json('this is the home of the server')
-})
-
-// Enable the routes in the app
-app.use('/api', repos)
-app.use('/api', stats)
-app.use('/blog', blog)
 
 module.exports = app;
